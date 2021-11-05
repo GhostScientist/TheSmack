b10cb79e906cc697ec8fbabb25690006d4b27b8d
Dakota Kim <codingvampire@gmail.com>
2021-11-05 14:03:59 -0400

    Initial Commit

---
+++ TheSmack/TheSmackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheSmackApp.swift
+//  TheSmack
+//
+//  Created by Dakota Kim on 11/5/21.
+//
+
+import SwiftUI
+
+@main
+struct TheSmackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
